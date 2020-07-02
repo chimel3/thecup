d9218d91b9978808cf54302547a6affb2df6604c
Tim Mitchell <tim_mitchell@outlook.com>
2020-07-02 16:09:31 +0100

    Match engine with initial start code that brings back the data for all of the teams still left in the competition

---
+++ game/matchengine.py
@@ -0,0 +1,35 @@
+import flask
+import os
+from azure.cosmosdb.table.tableservice import TableService
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+
+app = flask.Flask(__name__)
+
+@app.route('/start', methods=['POST'])
+def start_round():
+    matches = flask.request.get_json()    # requires header content-type of "application/json"
+    keyVaultName = os.environ["KEY_VAULT_NAME"]
+    keyVault_URI = "https://" + keyVaultName + ".vault.azure.net"
+
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url=keyVault_URI, credential=credential)
+    data_access_key = client.get_secret("thecupstore-key")
+
+    table_service = TableService(account_name='thecupstore', account_key=data_access_key.value)
+
+    query_string = ""
+    for match in matches:
+        for team in match:
+            query_string += "Name eq \'" + team + "\' or "
+
+    # Remove trailing ' or '
+    query_string = query_string[:-4]
+    
+    global teams
+    teams = table_service.query_entities('Teams', filter=query_string)
+    return '', 200
+
+if __name__ == "__main__":
+    app.run(port=1955, debug=True)
